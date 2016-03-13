ea2668e38f1702433727c39c047d14cc7c57a87f
Adam Israel <adam@adamisrael.com>
2016-03-13 01:40:43 -0500

    Publishing site on Sun 13 Mar 2016 01:40:43 EST.

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ hugo
 echo -e "Committing changes to Github..."
 git add -A
 git commit -m "Publishing site on `date`."
+git push origin master
 
 echo -e "Uploading..."
-rsync -arvz ./public/ aisrael@ftp.adamisrael.com:dev.adamisrael.com
+rsync -arz ./public/ aisrael@ftp.adamisrael.com:dev.adamisrael.com
