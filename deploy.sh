8207ba5916355fff541100c60d5ae4bb7835778f
Adam Israel <adam@adamisrael.com>
2022-11-14 19:10:33 -0500

    Update the deploy script
    Update the deploy script to not push directly to master, but to simply deploy what's current in git.

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 echo -e "Building page(s)"
 hugo
 
-echo -e "Committing changes to Github..."
-git add -A
-git commit -m "Publishing site on `date`."
-git push origin master
+# I've moved to a pull request-based workflow. At most, I might tag each
+# deployment with a timestamp.
+#echo -e "Committing changes to Github..."
+#git add -A
+#git commit -m "Publishing site on `date`."
+#git push origin master
 
 echo -e "Uploading..."
 rsync -arz ./public/ aisrael@ftp.adamisrael.com:adamisrael.com
